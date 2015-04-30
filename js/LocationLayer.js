6ff4487fe0a4c5140a6291b35f480fc7d264027d
Bert Spaan <bert.spaan@gmail.com>
2015-04-30 10:28:19 +0200

    triangle marker! closes #16

--- js/LocationLayer.js
+++ js/LocationLayer.js
@@ -1,6 +1,17 @@
+var markerSize = 30;
+var greenIcon = L.icon({
+    iconUrl: '../images/marker.svg',
+    iconSize: [markerSize, markerSize],
+    iconAnchor: [markerSize / 2, markerSize],
+    popupAnchor: [markerSize / 2, markerSize / 2]
+});
+
 L.LocationLayer = L.Marker.extend({
   initialize: function (latlng) {
-    options = L.setOptions(this, {draggable: true});
+    options = L.setOptions(this, {
+      draggable: true,
+      icon: greenIcon
+    });
     this._latlng = L.latLng(latlng);
   }
 });
